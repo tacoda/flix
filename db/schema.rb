0db0ac379e6ac02ec5ff3e45b3ac5f60f3c4e748
Ian Johnson <tacoda@pm.me>
2020-05-06 19:29:32 -0400

    Add new fields to the movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_200040) do
+ActiveRecord::Schema.define(version: 2020_05_06_231749) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_04_30_200040) do
     t.decimal "total_gross"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.date "released_on"
   end
 
 end

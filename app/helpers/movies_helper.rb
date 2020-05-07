ada3abbcce3053185542a15c359274cefa033f00
Ian Johnson <tacoda@pm.me>
2020-05-06 20:55:08 -0400

    Add view helpers for movies

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,15 @@
 module MoviesHelper
+
+    def total_gross(movie)
+        if movie.flop?
+            "Flop!"
+        else
+            number_to_currency(movie.total_gross, precision: 0)
+        end
+    end
+
+    def year_of(movie)
+        # movie.released_on.strftime("%Y")
+        movie.released_on.year
+    end
 end

fcf6eea48f364ec8a879cc26e178e3cd3c809d3c
Ian Johnson <tacoda@pm.me>
2020-05-08 21:32:24 -0400

    Add links and a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root "movies#index"
+
   get "movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movies/:id" => "movies#show", as: 'movie'
 end

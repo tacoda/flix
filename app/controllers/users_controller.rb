c9814da2c7d1948a346a7fd4d5720ae06a19e126
Ian Johnson <tacoda@pm.me>
2020-06-13 19:21:50 -0400

    Add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+	  @user = User.new(user_params)
+	  if @user.save
+	    redirect_to @user, notice: "Thanks for signing up!"
+	  else
+	    render :new
+	  end
+	end
+
+	private
+
+	def user_params
+	  params.require(:user).
+	    permit(:name, :email, :password, :password_confirmation)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :users
   root "movies#index"
 
   resources :movies do
   	resources :reviews
   end
+
+  resources :users
+  get "signup" => "users#new"
 end

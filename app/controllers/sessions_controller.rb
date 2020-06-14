db565d727def37834efa6c8dcd32a194a7a5828a
Ian Johnson <tacoda@pm.me>
2020-06-13 20:20:04 -0400

    Add authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		fail
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+		     redirect_to user, notice: "Welcome back, #{user.name}!"
+		else
+			flash.now[:alert] = "Invalid email/password combination!"
+		    render :new
+		end
 	end
 
 	def destroy

48c6eb18621930da98ff1efd759dac3a8471cf32
Ian Johnson <tacoda@pm.me>
2020-06-14 10:36:20 -0400

    Update views and functionality for new review workflow

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :require_signin
   before_action :set_movie
 
   def index
@@ -11,6 +12,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = @movie.reviews.new(review_params)
+    @review.user = current_user
 
     if @review.save
       redirect_to movie_reviews_path(@movie),
@@ -45,7 +47,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:name, :comment, :stars)
+    params.require(:review).permit(:comment, :stars)
   end
 
   def set_movie
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@reviews = @user.reviews
 	end
 
 	def new

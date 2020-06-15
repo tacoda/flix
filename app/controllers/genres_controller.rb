f3c1f50ec46b6fb3fe53f712169787241d2860ea
Ian Johnson <tacoda@pm.me>
2020-06-15 17:26:27 -0400

    Add genre resource controller and views

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,50 @@
 class GenresController < ApplicationController
+	before_action :require_signin, except: [:index, :show]
+    before_action :require_admin, except: [:index, :show]
+
+    def index
+      @genres = Genre.all
+    end
+
+    def show
+      @genre = Genre.find(params[:id])
+    end
+
+    def edit
+      @genre = Genre.find(params[:id])
+    end
+
+    def update
+      @genre = Genre.find(params[:id])
+      if @genre.update(genre_params)
+        redirect_to @genre, notice: 'Genre successfully updated!'
+      else
+        render :edit
+      end
+    end
+
+    def new
+      @genre = Genre.new
+    end
+
+    def create
+      @genre = Genre.new(genre_params)
+      if @genre.save
+        redirect_to @genre, notice: 'Genre successfully created!'
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @genre = Genre.find(params[:id])
+      @genre.destroy
+      redirect_to genres_url, alert: 'Genre successfully deleted!'
+    end
+
+    private
+
+    def genre_params
+      params.require(:genre).permit(:name)
+    end
 end

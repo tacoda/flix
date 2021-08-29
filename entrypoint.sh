e3757b21f9880a57b13350d5d2d3932c6d838b1e
Ian Johnson <ijohnson@covermymeds.com>
2021-08-29 11:31:10 -0400

    Add alternative to removing pid file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-DIR=/flix/tmp/pids
-FILE=/flix/tmp/pids/server.pid
-[ -d "$DIR" ] && [ -f "$FILE" ] && rm -f $FILE
+rm -f /flix/tmp/pids/server.pid || echo "No pre-existing server.pid"
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
